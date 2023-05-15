3617fb18227f6d904c4d0dcb9155d911c1218a66
himaloyash <himaloyashis98@gmail.com>
2023-05-15 10:38:15 +0600

    I'm quater done

--- Back-end and database-milsestone-11/simple-crud-client/src/main.jsx
+++ Back-end and database-milsestone-11/simple-crud-client/src/main.jsx
@@ -12,7 +12,6 @@ const router = createBrowserRouter([
 ])
 
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
